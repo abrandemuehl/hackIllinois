8e52cf882d0dadabdc1f2e3a10e987dd102bdeb9
Martin Pandola <martinpandola@gmail.com>
2016-02-21 02:21:11 -0600

    fixed mac address bugs

--- client/scrape.py
+++ client/scrape.py
@@ -21,12 +21,14 @@ def PacketHandler(pkt):
     global HANDSHAKE_DONE
     global ROUTER_DATA
     if pkt.haslayer(Dot11):
+        print(ROUTERS)
         # if pkt.type==PROBE_REQUEST_TYPE and pkt.subtype == PROBE_REQUEST_SUBTYPE and ( pkt.addr2.lower() in WHITELIST or pkt.addr2.upper() in WHITELIST):
         # if pkt.addr2.lower() in WHITELIST or pkt.addr2.upper() in WHITELIST or pkt.addr1.lower() in WHITELIST or pkt.addr1.lower() in WHITELIST:
         if len(ROUTERS)!=0:
             if pkt.addr2 and pkt.subtype!=8 and (pkt.addr2.lower() in ROUTERS or pkt.addr2.upper() in ROUTERS):
                 RouterPacket(pkt)
                 if len(ROUTER_DATA)>=2:
+                    print(ROUTER_DATA)
                     HANDSHAKE_DONE=True
                     ROUTERS=[]
         elif pkt.addr2 and pkt.subtype != 8:
@@ -46,11 +48,14 @@ def SigStrength(pkt):
 def RouterPacket(pkt):
     global ROUTER_DATA
     signal_strength = SigStrength(pkt)
+    print(signal_strength)
     ROUTER_DATA[pkt.addr2]=signal_strength
 
 def PrintPacket(pkt):
     signal_strength = SigStrength(pkt)
-    mac=hex(get_mac())
+    mac=get_mac()
+    mac=':'.join(("%12X" % mac)[i:i+2] for i in range(0,12,2))
+    mac=mac.lower()
     timestamp=int(time.time())
     print ("%s %s %s %s %s" % (sys.argv[1],timestamp, mac, pkt.addr2, signal_strength))
     
@@ -64,13 +69,16 @@ def handshake():
     global ROUTERS
     global HANDSHAKE_DONE
     global ROUTER_DATA
-    mac = hex(get_mac())
+    mac = get_mac()
+    mac=':'.join(("%012X" % mac)[i:i+2] for i in range(0,12,2))
+    mac=mac.lower()
     payload={'mac':mac}
     r = requests.post(URL_HANDSHAKE,data=payload)
     while(1):
         time.sleep(0.5)
         r = requests.get(URL_HANDSHAKE)
         data=r.json()
+        print(data)
         if(len(data)==3):
             break
     data=requests.get(URL_HANDSHAKE).json()
